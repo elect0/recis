d9d6d14dcfb5248d7e4a54623a4071ccbc904a5a
elect0 <educinca8@gmail.com>
2025-12-17 17:43:35 +0200

    feat: add bounds checking to RESP parser

--- include/parser.h
+++ include/parser.h
@@ -1,7 +1,7 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-int read_until_crlf(const char *buffer, int *pos, char *out, int max_len);
+int read_until_crlf(const char *buffer, int input_len, int *pos, char *out, int max_out_len);
 int parse_resp_request(char *buffer, int len, char **arg_values, int max_args);
 
 #endif // !PARSER_H
